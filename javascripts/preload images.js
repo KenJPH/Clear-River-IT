fb86b56df58f26de1acc925ab021dc6830bffbdf
sofossofos <sofossofos@users.noreply.github.com>
2016-04-25 15:16:04 +1000

    Added files via upload

---
+++ javascripts/preload images.js
@@ -0,0 +1,17 @@
+<div class="hidden">
+	<script type="text/javascript">
+		<!--//--><![CDATA[//><!--
+			var images = new Array()
+			function preload() {
+				for (i = 0; i < preload.arguments.length; i++) {
+					images[i] = new Image()
+					images[i].src = preload.arguments[i]
+				}
+			}
+			preload(
+				"http://clearriver.me/intro-bg.jpg",
+				"http://clearriver.me/downloads-bg.jpg",
+			)
+		//--><!]]>
+	</script>
+</div>
